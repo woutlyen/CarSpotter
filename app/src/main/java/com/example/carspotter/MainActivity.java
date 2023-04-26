5f7904bc3ceb340bfa7529fe97c62af07f60ed8d
wout.lyen <wout.lyen@student.kuleuven.be>
2023-04-26 20:50:13 +0200

    Added onClick functionality for Spotter button

--- app/src/main/java/com/example/carspotter/MainActivity.java
+++ app/src/main/java/com/example/carspotter/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.carspotter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnWiki;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnWiki = (Button) findViewById(R.id.btnWiki);
+    }
+
+    public void onBtnWiki_Clicked(View caller){
+        Intent intent =new Intent(this, BrandSelectActivity.class);
+        startActivity(intent);
     }
 }
