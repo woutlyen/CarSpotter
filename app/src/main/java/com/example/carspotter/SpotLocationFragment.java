4b7c6964a0a7e531c49cedf044afb1254e29b4b4
ItsAlphie <jvalgaeren1@gmail.com>
2023-05-10 00:06:54 +0200

    Added database connection in SpotLocationFragment (request spots from specific car_id)

--- app/src/main/java/com/example/carspotter/SpotLocationFragment.java
+++ app/src/main/java/com/example/carspotter/SpotLocationFragment.java
@@ -8,8 +8,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.carspotter.model.Car;
+import com.example.carspotter.model.Spot;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,13 +28,19 @@ import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
 import com.google.maps.android.heatmaps.HeatmapTileProvider;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SpotLocationFragment extends Fragment {
     private GoogleMap map;
     private HeatmapTileProvider provider;
     private TileOverlay overlay;
+    private static final String QUEUE_URL = "https://studev.groept.be/api/a22pt304/GetSpotsFromCarId";
+    private List<Spot> spots = new ArrayList<>();
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
         /**
@@ -43,33 +57,29 @@ public class SpotLocationFragment extends Fragment {
             //TODO: maak lijst van de spots en plaats deze als markers op de map.
             // De meest recente krijgt een andere tag (misschien best voor de andere ook de datum in de marker)
             // Voor de tags kunt ge uit de database "lat" en "long" halen voor bepaalde car_id
-            /*
-            //HEATMAP
-            addHeatMap(googleMap);
 
-            //MARKER
-            LatLng selectedSpot = new LatLng(50, 5);
-            googleMap.addMarker(new MarkerOptions().position(selectedSpot).title("Selected Spot"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(selectedSpot));
-            */
             map = googleMap;
             // Create a list of LatLng objects representing the heatmap data points
             ArrayList<LatLng> dataPoints = new ArrayList<>();
-            dataPoints.add(new LatLng(37.775, -122.419));
-            dataPoints.add(new LatLng(37.776, -122.418));
-            dataPoints.add(new LatLng(37.777, -122.417));
+            dataPoints.add(new LatLng(50, 5.001));
+            dataPoints.add(new LatLng(50, 5.002));
+            dataPoints.add(new LatLng(50.001, 5.001));
             // Add more data points as needed...
 
-            // Create a HeatmapTileProvider using the data points
+            //HEATPMAP:
+            // 1: Create a HeatmapTileProvider using the data points
             provider = new HeatmapTileProvider.Builder()
                     .data(dataPoints)
                     .build();
 
-            // Add the heatmap overlay to the map
+            // 2: Add the heatmap overlay to the map
             map.addTileOverlay(new TileOverlayOptions().tileProvider(provider));
 
+            //MARKER
             // Set the camera position to the center of the heatmap data points
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.776, -122.418), 13));
+            LatLng selectedSpot = new LatLng(50, 5);
+            map.addMarker(new MarkerOptions().position(selectedSpot).title("Selected Spot"));
+            map.moveCamera(CameraUpdateFactory.newLatLng(selectedSpot));
         }
     };
 
@@ -78,6 +88,11 @@ public class SpotLocationFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        Bundle bundle = this.getArguments();
+        Spot spot = bundle.getParcelable("Spot");
+
+        requestSpotsFromCarId(String.valueOf(spot.getCar_id()));
+
         return inflater.inflate(R.layout.fragment_spot_location, container, false);
     }
 
@@ -92,4 +107,54 @@ public class SpotLocationFragment extends Fragment {
     }
 
     //TODO: import all cars from car_id
+    private void requestSpotsFromCarId(String item) {
+        // Retrieve spots from database with Volley
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        JsonArrayRequest queueRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                QUEUE_URL+"/"+item,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        processJSONResponse(response);
+                        if (spots.size() != 0){
+                            Toast.makeText(
+                                    getActivity(),
+                                    "Succesfully received all spots from database",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(
+                                    getActivity(),
+                                    "error: there was an issue retreiving data from server",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(
+                                getActivity(),
+                                "Unable to communicate with the server",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+        requestQueue.add(queueRequest);
+    }
+
+    private void processJSONResponse(JSONArray response) {
+        //Add spots from database into local list for recyclerview
+        spots.clear();
+        for (int i = 0; i < response.length(); i++) {
+            try {
+                Spot spot = new Spot(response.getJSONObject(i));
+                spots.add(spot);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        Collections.sort(spots, (o1, o2) -> o1.getDate().compareTo(o2.getDate()));
+    }
 }
